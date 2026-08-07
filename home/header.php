60ca6ff0020d790ef07ed34f8f7a7b1e53ad6893
chaiy <chaiyifeng@gmail.com>
2026-08-07 21:34:14 +0800

    Add homepage design

---
+++ home/header.php
@@ -0,0 +1,15 @@
+<nav class="navbar">
+	    <a href="/TravelPal/" class="logo">
+        <img src="/TravelPal/images/logo.png" alt="TravelPal Logo">
+    </a>
+
+    <!-- 右侧 导航菜单 -->
+    <div class="nav-links">
+        <a href="/TravelPal/home/">Home</a>
+        <a href="#">Flights</a>
+        <a href="#">Hotels</a>
+        <a href="#">Restaurants</a>
+        <a href="#">Attractions</a>
+        <a href="#">My Trips</a>
+    </div>
+</nav>

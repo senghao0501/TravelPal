b6ebc021db8c07e5d580b9cd14427ef56bc8fbdb
wenbing371 <laubinbinlau88@gmail.com>
2026-08-24 19:48:00 +0800

    attraction css

--- attractions/index.php
+++ attractions/index.php
@@ -46,7 +46,7 @@ $placeholderImage = 'data:image/svg+xml;charset=UTF-8,' . rawurlencode($placehol
 include '../header.php';
 ?>
 
-<link rel="stylesheet" href="../css/modules/attractions.css">
+<link rel="stylesheet" href="../css/modules/attractions.css?v=20260824-2">
 <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"

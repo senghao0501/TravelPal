c2d30893dd030e5c9dc21c96f2cf723c28e61483
wenbing371 <laubinbinlau88@gmail.com>
2026-09-05 14:44:04 +0800

    attraction api

---
+++ attractions/config.local.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!defined('RAPIDAPI_KEY')) {
+    define('RAPIDAPI_KEY', 'b348aabdb3msh54e9d33b12be724p1a83a2jsnbda717b1c9df');
+}
+
+if (!defined('RAPIDAPI_HOST')) {
+    define('RAPIDAPI_HOST', 'booking-com15.p.rapidapi.com');
+}
